9e1b9cb322deef6d8fda89e3cd215933f0f07b72
vic-villicana <victor_u9@msn.com>
2022-01-12 10:42:58 -0800

    create new form field component

---
+++ src/components/Field.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class Field extends React.Component{
+    
+    static propTypes = {
+        placeholder: PropTypes.string,
+        name: PropTypes.string.isRequired,
+        value: PropTypes.string,
+        validate: PropTypes.func,
+        onChange: PropTypes.func.isRequired
+    };
+
+    state = {
+        value: this.props.value,
+        error: false
+    };
+
+    static getDerivedStateFromProps(nextProps) {
+        return {value: nextProps.value}
+    }
+
+    onChange = evt => {
+        const name = this.props.name
+        const value = evt.target.value
+        const error = this.props.validate ? this.props.validate(value) : false
+
+        this.setState({value, error})
+
+        this.props.changed({name, value, error})
+    } 
+
+    render(){
+        return (
+            <div>
+                <input
+                    placeholder={this.props.placeholder}
+                    value={this.state.value}
+                    onChange={this.onChange}
+                />
+                <span style={{color:'red'}}>{this.state.error}</span>
+            </div>
+        )
+    }
+}
+
+export default Field
