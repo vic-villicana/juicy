90cd1e92308905e0a0ae7c0d10cc4bf0dd9a541a
vic-villicana <victor_u9@msn.com>
2023-01-13 12:10:48 -0800

    write first react integration test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,37 @@
-import { render, screen } from '@testing-library/react';
+import { render as rtlRender, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
+
+import { Provider } from 'react-redux'
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk';
+import reducers from './reducers'
+
+import { debug } from 'console';
+import {MemoryRouter} from 'react-router-dom'
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const render = component => rtlRender(
+  <Provider store={createStore(reducers, applyMiddleware(thunk))}>
+    {component}
+  </Provider>
+)
+
+describe("Home page to Order Page", () => {
+  test('clicks button and renders the order page', () => {
+    render(<App />);
+    const theElement = screen.getAllByRole('link', {name:/order/i})
+    expect(theElement[0]).toBeInTheDocument()
+    userEvent.click(theElement[0])
+    const head = screen.getByText(/current location/i)
+    expect(head).toBeInTheDocument()
+  });
+})
+
+describe("Order Page", () => {
+  test("renders the menu", () => {
+
+  })
+})
+
+
